94445cbc76fbf8e4ce3e8eeffaa62f5038a0563d
Taras <45391115+prystupataras@users.noreply.github.com>
2024-12-26 09:32:09 +0200

    Initial Commit

---
+++ TabBarSwiftUI/TabBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarSwiftUIApp.swift
+//  TabBarSwiftUI
+//
+//  Created by Taras Prystupa on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
